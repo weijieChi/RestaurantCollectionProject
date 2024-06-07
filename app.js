b19992f2794c65c880a1100961e359c451ba929e
weijie <qoo77823@hotmail.com>
2024-06-07 15:03:21 +0800

    feat: create restaurants page, and controller

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+// 設定使用 Handlebars 做為樣板引擎
+app.set('view engine', 'hbs')
 app.use(routes)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
-// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
-// 設定使用 Handlebars 做為樣板引擎
-app.set('view engine', 'hbs')
 
 module.exports = app
