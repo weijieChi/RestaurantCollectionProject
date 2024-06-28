e123b1a3fc54519183c2847be8576c6f2d35eb84
weijie <qoo77823@hotmail.com>
2024-06-28 16:39:10 +0800

    feat: update seed files

--- migrations/20240626084739-add-category-id-to-restaurant.js
+++ migrations/20240626084739-add-category-id-to-restaurant.js
@@ -1,18 +1,16 @@
 'use strict'
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColum('Restaurants', 'category_id', {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {
-        model: 'Catagories',
+        model: 'Categories',
         key: 'id'
       }
     })
   },
-
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColum('Restaurants', 'category_id')
+    await queryInterface.removeColumn('Restaurants', 'category_id')
   }
 }
