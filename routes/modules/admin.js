9e69f686b59ced05ed7d480b682a201dd5917e7c
weijie <qoo77823@hotmail.com>
2024-07-05 00:59:06 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -20,7 +20,10 @@ router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
 // categories
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
