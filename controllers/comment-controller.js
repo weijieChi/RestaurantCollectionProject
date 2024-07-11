7d48e1868b857e0977264cc556299bffc6e3d757
weijie <qoo77823@hotmail.com>
2024-07-11 15:54:35 +0800

    feat: add delete comment for admin

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,6 +21,17 @@ const commentController = {
         res.redirect(`/restautants/${restaurantId}`)
       })
       .catch(err => next(err))
+  },
+  deleteComment: (req, res, next) => {
+    return Comment.findByPk(req.params.id)
+      .then(comment => {
+        if (!comment) throw new Error("Comment didn't exist!")
+        return comment.destroy()
+      })
+      .then(deletedComment => {
+        res.redirect(`/restaurant/${deletedComment.restaurantId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = commentController
