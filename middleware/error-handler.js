18c6071cfb1917e5c3328afb263d77c5b9ecf31f
weijie <qoo77823@hotmail.com>
2024-09-25 19:49:38 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,5 +7,20 @@ module.exports = {
     }
     res.redirect('back')
     next(err)
+  },
+  // api 專用的 error Handler
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
+    next(err)
   }
 }
